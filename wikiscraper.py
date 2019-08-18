eb6c12ea5efeac4223b88e710e9be82cf95166f5
kevin-m-he <53435469+kevin-m-he@users.noreply.github.com>
2019-08-18 09:52:53 -0400

    Update wikiscraper.py

--- wikiscraper.py
+++ wikiscraper.py
@@ -2,32 +2,36 @@ import wikipedia
 
 while True:
     searchitem = input('What would you like to search on Wikipedia?\n')
-    #wikipedia.search(searchitem,results=15) gives 15 results
-    lst = wikipedia.search(searchitem)
-    dic = dict()
+    # wikipedia.search(searchitem,results=10)
+    words = wikipedia.search(searchitem)
+    storedwords = dict()
     count = 0
     print('__________________________________________')
-    for i in lst:
+    # Appends wikipedia search summary into storedwords dictionary
+    for i in words:
         count = count+1
-        dic[i] = dic.get(i,0)+count
+        storedwords[i] = storedwords.get(i,0)+count
         print('Search Result '+str(count)+':',i)
 
     pagetitle = input('__________________________________________\n\nSelect which page summary you would like to see...\nEnter Search Result Number: ')
+    # Prompts user for search result number
     try:
         x = int(pagetitle)
     except:
         print('Please enter the search result number:')
         continue
     page = None
-    for val,key in dic.items():
+    # Prints the summary for the user
+    for val,key in storedwords.items():
         if x == key:
             page = val
     try:
         print('\n',wikipedia.summary(page))
     except:
         print('Could not find search summary')
+    # Prompts the user if they want to search again or quit
     quit = input('\nWould you like to search again? (yes/no)')
-    if quit == 'yes':
+    if quit.lower() == 'yes':
         continue
     else:
         print('\nThanks for Searching!')
